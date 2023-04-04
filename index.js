1c35f8c6cb3f0ac05b21a6388280bf2d1ea67ee8
cspooner1 <christopherspooner423@gmail.com>
2023-04-03 19:48:58 -0500

    update

--- index.js
+++ index.js
@@ -40,18 +40,28 @@ function getRandomProducts(arrayOfProducts) {
     console.log(randomIndex2);
     console.log(randomIndex3);
 
+   
+
     if (randomIndex1 === randomIndex2 || randomIndex2 === randomIndex3) {
         randomIndex1 = Math.floor(Math.random() * arrayOfProducts.length);
         randomIndex1 = Math.floor(randomIndex2);
         randomIndex2 = Math.floor(Math.random() * arrayOfProducts.length);
         randomIndex2 = Math.floor(randomIndex3);
     }
+    
+    while (randomIndex1 === randomIndex2){
+    randomIndex1 = Math.floor(Math.random() * arrayOfProducts.length)
+    }
+    while (randomIndex3 === randomIndex2){
+        randomIndex3 = Math.floor(Math.random() * arrayOfProducts.length)
+    }
 
-
-    let randomProduct1 = arrayOfProducts[randomIndex1]
+    while (randomIndex1 === randomIndex3){
+        randomIndex3 = Math.floor(Math.random() * arrayOfProducts.length)
+    }
+ let randomProduct1 = arrayOfProducts[randomIndex1]
     let randomProduct2 = arrayOfProducts[randomIndex2]
-    let randomProduct3 = arrayOfProducts[randomIndex3]
-
+    let randomProduct3 = arrayOfProducts[randomIndex3] 
     randomProducts.push(randomProduct1)
     randomProducts.push(randomProduct2)
     randomProducts.push(randomProduct3)
@@ -154,4 +164,29 @@ function endResults() {
     if (numberOfVotes >= 25){
         button.remove(); 
     }
+
+    const ctx = document.getElementById('myChart');
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: [products[0].productName, products[1].productName, products[2].productName, products[3].productName, products[4].productName,products[5].productName, products[6].productName, products[7].productName, products[8].productName, products[9].productName, products[10].productName, products[11].productName, products[12].productName, products[13].productName, products[14].productName, products[15].productName, products[16].productName, products[17].productName, products[18].productName, ],
+          datasets: [{
+            label: '# of Votes',
+            data: [products[0].clicks, products[1].clicks, products[2].clicks, products[3].clicks, products[4].clicks,products[5].clicks, products[6].clicks, products[7].clicks, products[8].clicks, products[9].clicks, products[10].clicks, products[11].clicks, products[12].clicks, products[13].clicks, products[14].clicks, products[15].clicks, products[16].clicks, products[17].clicks, products[18].clicks, ],
+            borderWidth: 1
+          }, {
+              label: '# of Times Shown',
+            data:[products[0].timesShown, products[1].timesShown, products[2].timesShown, products[3].timesShown, products[4].timesShown, products[5].timesShown, products[6].timesShown, products[7].timesShown, products[8].timesShown, products[9].timesShown, products[10].timesShown, products[11].timesShown, products[12].timesShown, products[13].timesShown, products[14].timesShown, products[15].timesShown, products[16].timesShown, products[17].timesShown, products[18].timesShown, ],
+            borderWidth: 1
+          }]
+      },
+      options: {
+          indexAxis: 'y',
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+      });
 }
